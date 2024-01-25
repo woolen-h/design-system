cdd2ca6760b5a2a088dc1d56337402e85908d744
WOOL <woolen_h@kakao.com>
2024-01-25 13:44:54 +0900

    [Add] cursor

---
+++ stories/Cursor.js
@@ -0,0 +1,3 @@
+import './assets/css/menu.css';
+
+export const Cursor_01 = () => '<div class="cursor-style">Cursor 01</div>';
---
+++ stories/Cursor.stories.js
@@ -0,0 +1,9 @@
+
+import * as Cursor from "./Cursor";
+import './assets/css/cursor.css';
+
+export default {
+    title: 'Components/Cursor'
+  };
+
+export const Cursor_01 = Cursor.Cursor_01;
