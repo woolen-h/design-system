2db89f1348991fd62e3a2067b0cb70683b5f4ccf
WOOL <woolen_h@kakao.com>
2024-01-25 13:49:55 +0900

    [Delete] Cursor

--- stories/Cursor.js
+++
@@ -1,3 +0,0 @@
-import './assets/css/menu.css';
-
-export const Cursor_01 = () => '<div class="cursor-style">Cursor 01</div>';
--- stories/Cursor.stories.js
+++
@@ -1,9 +0,0 @@
-
-import * as Cursor from "./Cursor";
-import './assets/css/cursor.css';
-
-export default {
-    title: 'Components/Cursor'
-  };
-
-export const Cursor_01 = Cursor.Cursor_01;
