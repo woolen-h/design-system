903e0878e419533450cb69b1b87b7e61a929c07e
woolen-h <woolen-h@users.noreply.github.com>
2024-01-26 04:30:16 +0000

    deploy: fdb7a3b04ca4f973db9550bd89c20f15d0ad8d65

--- storybook-static/assets/Alert-_bF1MFQB.js
+++
@@ -1,1 +0,0 @@
-const a=({state:t="",label:r="This is Alert!"})=>{const e=document.createElement("div");return e.className=[`alert alert-${t}`],e.innerText=r,e};export{a as D};
---
+++ storybook-static/assets/Icon_Alert.stories-xFcvYmP4.js
@@ -0,0 +1,65 @@
+import{I as v}from"./Alert--9bSLbzN.js";const B={title:"Alert/Icon Alert"},r=({state:j,...q})=>v({state:j,...q}),e=r.bind({});e.args={state:"",icon_name:"fi fi-ss-bell"};const s=r.bind({});s.args={state:"primary",icon_name:"fi fi-ss-bell"};const n=r.bind({});n.args={state:"success",icon_name:"fi fi-ss-bell"};const a=r.bind({});a.args={state:"danger",icon_name:"fi fi-ss-bell"};const t=r.bind({});t.args={state:"warning",icon_name:"fi fi-ss-bell"};const o=r.bind({});o.args={state:"info",icon_name:"fi fi-ss-bell"};const c=r.bind({});c.args={state:"dark",icon_name:"fi fi-ss-bell"};const i=r.bind({});i.args={state:"light",icon_name:"fi fi-ss-bell"};var l,m,g;e.parameters={...e.parameters,docs:{...(l=e.parameters)==null?void 0:l.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(g=(m=e.parameters)==null?void 0:m.docs)==null?void 0:g.source}}};var u,d,p;s.parameters={...s.parameters,docs:{...(u=s.parameters)==null?void 0:u.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(p=(d=s.parameters)==null?void 0:d.docs)==null?void 0:p.source}}};var I,_,f;n.parameters={...n.parameters,docs:{...(I=n.parameters)==null?void 0:I.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(f=(_=n.parameters)==null?void 0:_.docs)==null?void 0:f.source}}};var A,b,S;a.parameters={...a.parameters,docs:{...(A=a.parameters)==null?void 0:A.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(S=(b=a.parameters)==null?void 0:b.docs)==null?void 0:S.source}}};var D,h,k;t.parameters={...t.parameters,docs:{...(D=t.parameters)==null?void 0:D.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(k=(h=t.parameters)==null?void 0:h.docs)==null?void 0:k.source}}};var y,x,L;o.parameters={...o.parameters,docs:{...(y=o.parameters)==null?void 0:y.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(L=(x=o.parameters)==null?void 0:x.docs)==null?void 0:L.source}}};var P,W,w;c.parameters={...c.parameters,docs:{...(P=c.parameters)==null?void 0:P.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(w=(W=c.parameters)==null?void 0:W.docs)==null?void 0:w.source}}};var E,O,T;i.parameters={...i.parameters,docs:{...(E=i.parameters)==null?void 0:E.docs,source:{originalSource:`({
+  state,
+  ...args
+}) => {
+  return Alert.IconAlert({
+    state,
+    ...args
+  });
+}`,...(T=(O=i.parameters)==null?void 0:O.docs)==null?void 0:T.source}}};const C=["Icon_Default","Icon_Primary","Icon_Success","Icon_Danger","Icon_Warning","Icon_Info","Icon_Dark","Icon_Light"];export{a as Icon_Danger,c as Icon_Dark,e as Icon_Default,o as Icon_Info,i as Icon_Light,s as Icon_Primary,n as Icon_Success,t as Icon_Warning,C as __namedExportsOrder,B as default};
