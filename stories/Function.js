98a1b978abae3ece22bea00fcd813c47c874f38c
WOOL <wobada@naver.com>
2024-01-28 15:37:08 +0900

    [Add] Function, Progress 코드 분석

--- stories/Function.js
+++ stories/Function.js
@@ -5,16 +5,25 @@ export const generateOTP = () => {
     document.addEventListener('DOMContentLoaded', () => {
         function generate() {
         let OTP = '';
+        // OTP의 값 스트링으로 기본 지정
         for(let i=1; i<=6; i++) {
+            // 1부터 6까지의 값을 반복하는 루프를 정의
             OTP += Math.floor(Math.random() * 10);
+            // Math.random() 함수는 0 이상 1 미만의 임의의 실수를 생성
+            // * 10을 하면 0 이상 10 미만의 값을 생성하게 됨 (소수점 포함)
+            // Math.floor() 함수를 사용하면 해당 값을 내림하여 소수점을 제거하고 0부터 9 사이의 정수를 얻을 수 있음
         }
 
         document.querySelector('#otpTextBox').value = OTP;
+        // ID가 otpTextBox인 요소를 선택하여 해당 요소의 value 속성에 OTP 변수 값을 할당
     }
 
     generate();
+    // 스크립트가 실행될 때 generate 함수를 호출하여 초기 OTP를 생성하고 해당 값을 otpTextBox에 할당
 
     document.querySelector('#generateButton').addEventListener('click', generate);
+    // ID가 generateButton인 요소를 선택하고 해당 요소에 click 이벤트 리스너를 추가
+    // 이벤트가 발생하면 generate 함수가 호출되어 새로운 OTP가 생성되고 화면에 표시됨
     });
 
     return '<div class="otp"><input type="text" id="otpTextBox" readonly /><button class="btn primary-btn" id="generateButton">Generate</button></div>'
--- stories/Progress.js
+++ stories/Progress.js
@@ -3,45 +3,69 @@ import './assets/css/progress.css';
 export const StepProgressBar = () => {
     document.addEventListener('DOMContentLoaded', () => {
         const bar = document.querySelector('#bar');
+        // progress bar 라인 선택
         const steps = document.querySelectorAll('.progress-step');
+        // progress bar 단계를 전체 선택 - 노드로 호출
         const btnPrev = document.querySelector('#prev');
+        // 이전 버튼 선택
         const btnNext = document.querySelector('#next');
+        // 다음 버튼 선택
 
         let currActive = 1;
+        // 1단계로 초기화 설정
 
         btnNext.addEventListener("click", () => {
+            // 다음 버튼 클릭 시 이벤트리스너
             currActive++;
+            // 활성화 단계 수 추가
 
             if (currActive > steps.length) {
+                // 활성화 단계 수가 총 단계 수를 넘어설 때,
             currActive = steps.length;
+                // 활성화 단계 수를 총 단계 수와 같게 설정하여 예외 방지
             }
 
             updateSteps();
+            // 아래 업데이트 펑션 불러오기
         });
 
         btnPrev.addEventListener("click", () => {
+            // 이전 버튼을 클릭할 시 이벤트리스너
             currActive--;
+            // 클릭할 때마다 활성화 단계 수를 줄임
 
             if (currActive < 1) {
+                // 활성화 단계 수가 1보다 작아질 때
                 currActive = 1;
+                // 1로 설정하여 예외 방지
             }
 
             updateSteps();
+            // 아래 업데이트 펑션 불러오기
         });
 
         const updateSteps = () => {
+            // 프로그레스 바의 상태를 업데이트
             steps.forEach((step, index) => {
+                // 각 단계를 반복함 단계와 각 인덱스를 변수로 사용
                 if (index < currActive) step.classList.add("active");
+                // 활성화된 단계보다 작은 인덱스를 갖는 단계는 active 클래스 추가
                 else step.classList.remove("active");
+                // 나머지는 active 클래스 제거
             });
 
             const actives = document.querySelectorAll(".active");
+            // active 클래스 모든 요소를 선택하여 actives 변수로 저장
 
             bar.style.width = ((actives.length - 1) / (steps.length - 1)) * 100 + "%";
-
+            // bar의 너비를 계산하여 업데이트, active 클래스가 지정된 단계의 수를 전체 단계의 수로 나눈 비율을 계산하여 백분율로 변환
+            // 프로그레스 바의 넓이를 설정한다.
             if (currActive === 1) btnPrev.disabled = true;
+            // 현재 단계가 첫번째 단계이면 이전 버튼을 비활성화
             else if (currActive === steps.length) btnNext.disabled = true;
+            // 현재 단계가 마지막 단계이면 다음 버튼을 비활성화
             else {
+                // 그렇지 않은 경우, 두 버튼 모두 활성화 상태로 유지
                 btnNext.disabled = false;
                 btnPrev.disabled = false;
             }
